0db28f7cc171702a6c9c437018304ab216e22a9c
Gabriele Bologna <bolognagabriele2003@gmail.com>
2022-08-16 20:51:37 +0200

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ProtoRandom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GabryB03")]
+[assembly: AssemblyProduct("ProtoRandom")]
+[assembly: AssemblyCopyright("Copyright © GabryB03 2022")]
+[assembly: AssemblyTrademark("Copyright © GabryB03 2022")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8c591c2a-3c3f-4364-a9e2-70a07bf0bccb")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
