2d8d24fdbb9deb5fb5af99c37b6e342bd99105fc
ZygoteCode <zygotecode@proton.me>
2025-04-20 21:56:07 +0200

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ProtoRandom")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ZygoteCode")]
+[assembly: AssemblyProduct("ProtoRandom")]
+[assembly: AssemblyCopyright("Copyright © ZygoteCode 2025")]
+[assembly: AssemblyTrademark("Copyright © ZygoteCode 2025")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8c591c2a-3c3f-4364-a9e2-70a07bf0bccb")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
